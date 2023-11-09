0f24ba0b5478f3ca7005e4b8adb3de819f3ac284
LuigiantonioIacomino <iacominoluigi54@gmail.com>
2023-11-09 22:02:04 +0100

    esercizio finito

---
+++ js/script.js
@@ -0,0 +1,28 @@
+let nome=prompt("inserisci il tuo nome:");
+let cognome=prompt("inserisci il tuo cognome:");
+let colorePreferito=prompt("inserisci il tuo colore preferito:");
+
+let casuale=Math.floor(Math.random() * 11);
+
+
+document.getElementById('pass').innerHTML=nome+cognome+colorePreferito+casuale;
+
+
+
+
+
+
+/*
+DIVISIONE
+let primoNumero=prompt("inserisci il primo numero:");
+let secondoNumero=prompt("inserisci il secondo numero:");
+let divisione=primoNumero / secondoNumero;
+*/
+
+
+
+/*
+STAMPO IN CONSOLE
+console.log(nome,cognome,colorePreferito,divisione);
+*/
+
